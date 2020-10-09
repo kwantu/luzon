70f9e80c06ac2a256c7a35624dca7ef4f0ecd200
syedhasanabbaszaidi <syedhasanabbaszaidi@gmail.com>
2020-10-09 17:01:05 +0530

    Kwantu Home page
    Created for styling purposes

--- pages/kwantu-home.js
+++ pages/kwantu-home.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
-import Navbar from '../components/Layout/Navbar';
-import Banner from '../components/HomeTwo/Banner';
-import Features from '../components/HomeOne/Features';
-import AboutUs from '../components/HomeTwo/AboutUs';
-import Services from '../components/HomeTwo/Services';
-import FeaturesTwo from '../components/HomeTwo/FeaturesTwo';
-import WorkingProcess from '../components/HomeTwo/WorkingProcess';
-import WhyChooseUs from '../components/HomeTwo/WhyChooseUs';
-import FunFacts from '../components/Common/FunFacts';
-import Projects from '../components/HomeTwo/Projects';
-import TeamStyleOne from '../components/Common/TeamStyleOne';
-import SkillsArea from '../components/HomeTwo/SkillsArea';
-import BlogPost from '../components/Common/BlogPost';
-import FeedbackStyleTwo from '../components/Common/FeedbackStyleTwo';
-import PartnerStyleOne from '../components/Common/PartnerStyleOne';
-import Footer from '../components/Layout/Footer';
 
-class Index2 extends Component {
+import Navbar from '../components/Layout/Navbar';
+import Footer from '../components/Layout/Footer';
+import BlogPost from '../components/Common/BlogPost';
+import DigitalExperience from '../components/Common/DigitalExperience';
+import BannerSlider from '../components/HomeOne/BannerSlider';
+import ServiceDevelopment from '../components/HomeOne/ServiceDevelopment';
+import Banner from '../components/HomeThree/Banner';
+
+class KwantuHome extends Component {
     render() {
         return (
             <React.Fragment>
-               <div> hello test </div>
-                <AboutUs />
-                
+                <Navbar />
+                <BannerSlider />
+                <DigitalExperience />
+                <ServiceDevelopment />
+                <Banner />
+                <BlogPost />
+                <Footer />
             </React.Fragment>
         );
     }
 }
 
-export default Index2;
+export default KwantuHome;
