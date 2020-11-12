26385743a6d12b1aa80f52680dd06fc218625585
Divvie Esterhuizen <divvieesterhuizen@Divvies-MacBook-Air-2017.local>
2020-11-12 12:36:15 +0200

    mobile - products page - video

--- components/TEA/TabsWithHeadings.js
+++ components/TEA/TabsWithHeadings.js
@@ -76,83 +76,24 @@ class TabsWithHeadings extends Component {
                         <div className="tab_content">
                             <div id="tab11" className="tabs_item">
                                 <div className="row">
-                                    <div className="col-lg-8">
-                                        <h2>One Lorem ipsum dolor sit amet, consectetur</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                                        <br className="mt-5"></br>
-                                        <ul className="red-arrow-list">
-                                            <li>Lorem ipsum dolor sit amet, consectetur, consectetur adipiscing elit, sed do, consectetur adipiscing elit, sed do</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                        </ul>
-                                        <br className="mt-5"></br>
-                                        <div className="banner-btn animated fadeInUp">
-                                                <Link href="/">
-                                                    <a className="default-btn">
-                                                        Find out more
-                                                        <i className="fa fa-chevron-right" aria-hidden="true"></i>
-                                                    </a>
-                                                </Link>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-4">
-                                        <img src={require("../../images/white-placeholder-block.png")} alt="image" />
+                                    <div className="col px-5">
+                                        <img src={require("../../images/white-placeholder-block-wide.png")} alt="image" />
                                     </div>
                                 </div>     
                             </div>
 
                             <div id="tab22" className="tabs_item">
                                 <div className="row">
-                                    <div className="col-lg-8">
-                                        <h2>Two Lorem ipsum dolor sit amet, consectetur</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                                        <br className="mt-5"></br>
-                                        <ul className="red-arrow-list">
-                                            <li>Lorem ipsum dolor sit amet, consectetur, consectetur adipiscing elit, sed do, consectetur adipiscing elit, sed do</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                        </ul>
-                                        <br className="mt-5"></br>
-                                        <div className="banner-btn animated fadeInUp">
-                                                <Link href="/">
-                                                    <a className="default-btn">
-                                                        Find out more
-                                                        <i className="fa fa-chevron-right" aria-hidden="true"></i>
-                                                    </a>
-                                                </Link>
-                                        </div>
-                                    </div>
-
-                                    <div className="col-lg-4">
-                                        <img src={require("../../images/white-placeholder-block.png")} alt="image" />
+                                    <div className="col px-5">
+                                        <img src={require("../../images/white-placeholder-block-wide.png")} alt="image" />
                                     </div>
                                 </div>     
                             </div>
 
                             <div id="tab33" className="tabs_item">
                                 <div className="row">
-                                    <div className="col-lg-8">
-                                        <h2>Three Lorem ipsum dolor sit amet, consectetur</h2>
-                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>
-                                        <br className="mt-5"></br>
-                                        <ul className="red-arrow-list">
-                                            <li>Lorem ipsum dolor sit amet, consectetur, consectetur adipiscing elit, sed do, consectetur adipiscing elit, sed do</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                            <li>Lorem ipsum dolor sit amet, consectetur</li>
-                                        </ul>
-                                        <br className="mt-5"></br>
-                                        <div className="banner-btn animated fadeInUp">
-                                                <Link href="/">
-                                                    <a className="default-btn">
-                                                        Find out more
-                                                        <i className="fa fa-chevron-right" aria-hidden="true"></i>
-                                                    </a>
-                                                </Link>
-                                        </div>
+                                    <div className="col px-5">
+                                        <img src={require("../../images/white-placeholder-block-wide.png")} alt="image" />
                                     </div>
 
                                     <div className="col-lg-4">
